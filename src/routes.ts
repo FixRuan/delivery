67e65a7c53f5465736f53f8e5807f5c625ddad19
fixruan <ruangoio01@gmail.com>
2022-12-17 15:52:32 -0300

    Create Deliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
+
 import {AuthenticateClientController} from "./modules/account/authenticateClient/useCases/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import {CreateDeliverymanController} from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 const routes = Router();
 
-routes.post("/client/", createClientController.handle);
+routes.post("/client", createClientController.handle);
 routes.post("/authenticate", authenticateClientController.handle);
-
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export {routes};
+
