712457930dc8d71fdbf3c63e29e71012a587582b
fixruan <ruangoio01@gmail.com>
2022-12-17 14:35:17 -0300

    Express Async errors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
-import express from "express";
+import "express-async-errors";
+import express, {NextFunction, Request, Response} from "express";
 import {routes} from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World"
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return res.status(400).json({
+      message: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
   });
-})
+});
 
 app.listen(3000, () => console.log("Server is running"));
